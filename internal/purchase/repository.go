57c79019ce0d3e0da46dfdd6c29675b0dad7ce15
Piotr Tutak <piotr.tutak@getindata.com>
2023-09-06 09:31:19 +0200

    repo decouple

--- internal/purchase/repository.go
+++ internal/purchase/repository.go
@@ -1,7 +1,64 @@
 package purchase
 
-import "context"
+import (
+	coffeeco "coffeeco/internal"
+	"coffeeco/internal/payment"
+	"coffeeco/internal/store"
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Rhymond/go-money"
+	"go.mongodb.org/mongo-driver/internal/uuid"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type Repository interface {
 	Store(ctx context.Context, purchase *Purchase) error
+}
+
+type MongoRepository struct {
+	purchases *mongo.Collection
+}
+
+func NewMongoRepo(ctx context.Context, connectionString string) (*MongoRepository, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		return nil, err
+	}
+	return &MongoRepository{
+		purchases: client.Database("coffeeco").Collection("purchases"),
+	}, nil
+}
+
+func (mongoRepo *MongoRepository) Store(ctx context.Context, purchase Purchase) error {
+	mongoDocument := toMongoPurchase(purchase)
+	_, err := mongoRepo.purchases.InsertOne(ctx, mongoDocument)
+	if err != nil {
+		return fmt.Errorf("failed to persist purchase: %w", err)
+	}
+	return nil
+}
+
+type mongoPurchase struct {
+	id                 uuid.UUID
+	store              store.Store
+	productsToPurchase []coffeeco.Product
+	total              money.Money
+	paymentMeans       payment.Means
+	timeOfPurchase     time.Time
+	cardToken          *string
+}
+
+func toMongoPurchase(purchase Purchase) mongoPurchase {
+	return mongoPurchase{
+		id:                 purchase.id,
+		store:              purchase.Store,
+		productsToPurchase: purchase.ProductsToPurchase,
+		total:              purchase.total,
+		paymentMeans:       purchase.PaymentMeans,
+		timeOfPurchase:     purchase.timeOfPurchase,
+		cardToken:          purchase.CardToken,
+	}
 }
