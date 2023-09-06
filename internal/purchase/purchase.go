e17d68d913bfdcac17769d42224644aef1ac2f50
Piotr Tutak <piotr.tutak@getindata.com>
2023-09-06 09:12:18 +0200

    repo and purchase

--- internal/purchase/purchase.go
+++ internal/purchase/purchase.go
@@ -4,6 +4,7 @@ import (
 	coffeeco "coffeeco/internal"
 	"coffeeco/internal/payment"
 	store "coffeeco/internal/store"
+	"context"
 	"errors"
 	"time"
 
@@ -31,5 +32,39 @@ func (p *Purchase) validateAndEnrich() error {
 		newTotal, _ := p.total.Add(&product.BasePrice)
 		p.total = *newTotal
 	}
+	if p.total.IsZero() {
+		return errors.New("total purchase cannot be zero")
+	}
+	p.id = uuid.New()
+	p.timeOfPurchase = time.Now()
+	return nil
+}
 
+type CardChargeService interface {
+	ChargeCard(ctx context.Context, amount money.Money, cardToken string) error
+}
+
+type Service struct {
+	cardService  CardChargeService
+	purchaseRepo Repository
+}
+
+func (s Service) CompletePurchase(ctx context.Context, purchase *Purchase) error {
+	if err := purchase.validateAndEnrich(); err != nil {
+		return err
+	}
+	switch purchase.PaymentMeans {
+	case payment.MEANS_CARD:
+		if err := s.cardService.ChargeCard(ctx, purchase.total, *purchase.CardToken); err != nil {
+			return errors.New("error charging card")
+		}
+	case payment.MEANS_CASH:
+		// Payed by cash, nothing to do
+	default:
+		return errors.New("invalid payment means")
+	}
+	if err := s.purchaseRepo.Store(ctx, purchase); err != nil {
+		return errors.New("error storing purchase")
+	}
+	return nil
 }
